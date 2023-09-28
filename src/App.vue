bdda52ca8489306ed260b80aa08e7290a8ed6648
Axel-Segovia19 <14segovia@gmail.com>
2023-09-27 20:23:56 -0600

    changed project into a pokedex, instead of a pokemon card it is now a pokedex. still need to add more features like pokemon info when a pokemon is selected and a navbar that states what we are looking at

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,73 @@
-
 <template>
-  <div class="flex flex-col justify-center items-center h-screen w-screen">
-    <pokemon-card/>
+  <div class="flex flex-col md:flex-row bg-slate-200">
+    <div class="w-full flex justify-center items-center">
+      <pokemon-card class="mr-4" :image="pokemonImage" />
+    </div>
+    <ul class="h-screen overflow-y-auto w-full md:overscroll-none p-4">
+      <li
+        v-for="(pokemon, index) in pokemonStore.allPokemon"
+        :key="`poke-${index}`"
+        @mouseover="setSelectedPokemon(pokemon)"
+      >
+        <div
+          class="flex items-center rounded-tl-full rounded-bl-full pl-2 gap-x-2 mt-2 hover:bg-blue-300"
+        >
+          <span>
+            <img
+              class="w-12 h-12"
+              :src="getListSprite(pokemon)"
+              alt="pokemonSprite"
+            />
+          </span>
+          <span>
+            No.{{ pokemon.entry_number }}
+            {{ getPokemonName(pokemon.pokemon_species.name) }}
+          </span>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup lang="ts">
-import PokemonCard from './components/PokemonCard.vue'
+import PokemonCard from "./components/PokemonCard.vue";
+import { onMounted, reactive, ref, Ref, computed, ComputedRef } from "vue";
+import axios from "axios";
 
+const selectedPokemon: Ref<string | undefined> = ref(undefined);
+const pokemonStore = reactive({
+  allPokemon: [],
+});
 
+const pokemonImage: ComputedRef<string> = computed(() => {
+  if (selectedPokemon.value) {
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${selectedPokemon.value.entry_number}.png` as string;
+  }
+  return "";
+});
+
+async function getAllPokemon(): Promise<void> {
+  const allPokemon = await axios.get(`https://pokeapi.co/api/v2/pokedex/2`);
+  allPokemon.data.pokemon_entries.forEach(async (entry) => {
+    const speciesData = await axios.get(entry.pokemon_species.url);
+    entry.pokemon_species.info = speciesData.data;
+  });
+  pokemonStore.allPokemon = allPokemon.data.pokemon_entries;
+}
+
+function getPokemonName(name: string): string {
+  return name[0].toUpperCase() + name.substring(1);
+}
+
+function setSelectedPokemon(pokemon): void {
+  selectedPokemon.value = pokemon;
+}
+
+function getListSprite(pokemonEntry: number): string {
+  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonEntry.entry_number}.png`;
+}
+
+onMounted(() => {
+  getAllPokemon();
+});
 </script>
--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,57 +1,24 @@
 <template>
-    <div class="w-full min-w-fit h-fit flex justify-center"  @click="getRandomPokemon">
-  <div
-    class="border-2 rounded-3xl p-4 [@media(min-width:400px)]:p-6 bg-yellow-400 max-w-lg"
-  >
-    <div class=" bg-purple-500 container">
-      <div v-if="pokemon" >
-        <header class="flex items-baseline justify-between mx-3.5 pt-2">
-          <h1 class="[@media(min-width:400px)]:text-3xl font-bold my-2">{{ pokemonName[0].toUpperCase()+pokemonName.slice(1) }}</h1>
-          <div class="flex items-center">
-            <h2 class="[@media(min-width:400px)]:text-3xl font-semibold">130 HP</h2>
-            <div class="w-6 h-6 [@media(min-width:400px)]:w-8 [@media(min-width:400px)]:h-8 border border-transparent ml-2 rounded-full bg-purple-400"></div>
+          <div class="rounded-lg">
+            <img  :src="computedImage" alt="Pokemon" class="max-w-full h-auto max-h-96">
           </div>
-        </header>
-        <section class="flex flex-col gap-y-2">
-          <div class="border-8 mx-4 border-yellow-400 flex justify-center shadow-[5px_5px_15px_rgba(0,0,0,0.8)]">
-            <img :src="pokemonImage" alt="Some Pokemon"/>
-          </div>
-          <div class="bg-yellow-200 rounded w-fit self-center px-1 font-semibold">
-                NO. {{ pokemon.id }} HT: 6'07" WT: {{ pokemon.weight }}.0 lbs.
-          </div>
-        </section>
-      </div>
-      <div v-else>
-        ...loading
-      </div>
-    </div>
-  </div>
-</div>
-
 </template>
 <script setup lang="ts">
-import { ref, onMounted, computed, ComputedRef } from "vue";
-import axios from "axios";
-
-const pokemon = ref();
-
-const pokemonImage: ComputedRef<string> = computed(
-  () => pokemon.value.sprites.other["official-artwork"]["front_default"]
-);
-const pokemonName: ComputedRef<string> = computed(() => pokemon.value.name);
-
-const getRandomPokemon = async () => {
-  const randomID: number = Math.floor(Math.random() * 1018) + 1;
-  const { data } = await axios.get(
-    `https://pokeapi.co/api/v2/pokemon/${randomID}`
-  );
-  pokemon.value = data;
-  console.log(pokemon.value);
-};
-
-onMounted(() => getRandomPokemon());
-const name = "PokemonCard";
+import questionMark from '../assets/questionMark.png'
+import { computed, ComputedRef } from 'vue';
 defineOptions({
-  name,
+  name: 'PokemonCard',
 });
+const props = defineProps({
+  image: {
+    type: String
+  }
+})
+const computedImage: ComputedRef<string> = computed(()=> props.image || questionMark)
 </script>
+
+<style scoped>
+.image-background{
+  background: repeating-linear-gradient(180deg, white 0%,white 5%, #ADD8E6 5%, #ADD8E6 10%);
+}
+</style>
